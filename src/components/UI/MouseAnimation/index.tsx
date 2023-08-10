3b2672b2933927c2fc6d4290ef27fd88c820be21
Vitor Régis <vitorregisrr@gmail.com>
2023-08-10 20:42:33 -0300

    Add theme color to MouseAnimation component

--- src/components/UI/MouseAnimation/index.tsx
+++ src/components/UI/MouseAnimation/index.tsx
@@ -12,18 +12,19 @@ const MouseAnimationWrapper = styled.div`
   .label {
     font-size: 12px;
     font-family: 'Roboto', sans-serif;
-    color: #fff;
+    color: ${(props) => props.theme.colors.background_opos};
     margin-left: 10px;
     text-transform: uppercase;
     opacity: 0.8;
     min-width: 200px;
+    transition: color 0.3s ease;
   }
 
   .mousey {
     width: 2px;
     padding: 10px 12px;
     height: 28px;
-    border: 2px solid #fff;
+    border: 2px solid ${(props) => props.theme.colors.background_opos};
     border-radius: 25px;
     opacity: 0.75;
     box-sizing: content-box;
@@ -32,7 +33,7 @@ const MouseAnimationWrapper = styled.div`
     width: 3px;
     height: 10px;
     border-radius: 25%;
-    background-color: #fff;
+    background-color: ${(props) => props.theme.colors.background_opos};
     animation-name: scroll;
     animation-duration: 2.2s;
     animation-timing-function: cubic-bezier(0.15, 0.41, 0.69, 0.94);
